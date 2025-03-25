4ee02d16fe4bc1d8edf82c353d787da698f0e91f
Niklas Simakov <niklas.simakov@tum.de>
2025-03-25 16:18:38 +0100

    Fix tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,19 @@ const { heroui } = require("@heroui/react");
 
 /**
  * Please note that Hero UI currently only works if you use the config file.
- * With tailwindcss v4 you would normally use your css files to set all the required config variables.
- * Therefore please also check the globals.css and configure the variables there.
+ * With tailwindcss v4 you would normally use your css files to set all the required config variables, 
+ * but this is not possible with Hero UI.
  */
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
   darkMode: "class",
   plugins: [heroui()],
 };
