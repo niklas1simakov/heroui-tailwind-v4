51453862995df3c58bcbd0e6641cdb8204004e63
Niklas Simakov <niklas.simakov@tum.de>
2025-04-03 18:36:44 +0200

    Add theme switch to demo page

--- setup.sh
+++ setup.sh
@@ -130,22 +130,26 @@ EOF
 cat << 'EOF' > src/app/page.tsx
 "use client";
 
+import { ThemeSwitch } from "@/components/ThemeSwitch";
 import { Button } from "@heroui/react";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-4 text-4xl font-bold">Hello World</h1>
-      <p className="mb-4 text-lg">
-        This is a simple example of how to use the new beta version of Hero UI with Tailwind CSS v4.
-      </p>
-      <div className="flex flex-row gap-4">
-        <Button variant="shadow" color="primary">
-          Click me
-        </Button>
-        <Button variant="bordered" color="default">
-          Test
-        </Button>
+    <div>
+      <ThemeSwitch className="absolute top-8 right-8" />
+      <div className="flex h-screen flex-col items-center justify-center">
+        <h1 className="mb-4 text-4xl font-bold">Hello World</h1>
+        <p className="mb-4 text-lg">
+          This is a simple example of how to use the new beta version of Hero UI with Tailwind CSS v4.
+        </p>
+        <div className="flex flex-row gap-4">
+          <Button variant="shadow" color="primary">
+            Click me
+          </Button>
+          <Button variant="bordered" color="default">
+            Test
+          </Button>
+        </div>
       </div>
     </div>
   );
